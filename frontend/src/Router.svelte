92bd711e7855e98cec87844c7314e9e290df4b70
Bartłomiej Strama <kontakt@bstrama.com>
2024-09-21 19:43:46 +0200

    Better routing

--- frontend/src/Router.svelte
+++ frontend/src/Router.svelte
@@ -3,7 +3,15 @@
 
     export type PageParam = "" | "gauge";
 
-    export let page = writable<PageParam>("");
+    export let page = writable<PageParam>(null);
+
+    function removeEmptyParams() {
+        const url = new URL(window.location.href);
+        if (url.searchParams.get('page') === "") {
+            url.searchParams.delete('page');
+            window.history.pushState({}, '', url.toString());
+        }
+    }
 
     export function setPage(newPage?: PageParam) {
         newPage = newPage || "";
@@ -12,26 +20,25 @@
         url.searchParams.set('page', newPage);
         window.history.pushState({}, '', url.toString());
         page.set(newPage);
+        removeEmptyParams();
     }
 </script>
 <script lang="ts">
     import favIcon from "./assets/favicon.png";
-    import {onMount} from "svelte";
 
     import IndexApp from "../index-src/App.svelte";
     import GaugeApp from "../gauge-src/App.svelte";
 
     function updatePage() {
-        const newPage = new URLSearchParams(window.location.search)
+        const newPage = new URLSearchParams(window.location.search || '?page=')
             .get('page')
             .toLowerCase() as PageParam;
         page.set(newPage);
     }
 
-
-        updatePage();
-        window.addEventListener('popstate', updatePage);
-
+    updatePage();
+    window.addEventListener('popstate', updatePage);
+    removeEmptyParams();
 </script>
 <svelte:head>
     <link rel="icon" type="image/svg+xml" href={favIcon}/>
